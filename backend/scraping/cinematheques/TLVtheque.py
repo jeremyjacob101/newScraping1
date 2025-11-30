48063e8c2366aab8dbc9128036f6ee3bd4f5d688
JJ <jeremyjacob101@gmail.com>
2025-11-30 17:20:37 +0200

    tlv fixed temporarily? maybe make a div[3/5] switcher

--- backend/scraping/cinematheques/TLVtheque.py
+++ backend/scraping/cinematheques/TLVtheque.py
@@ -17,23 +17,16 @@ class TLVtheque(BaseCinema):
             date_string = current_date.strftime("%Y-%m-%d")
             self.driver.get(f"https://www.cinema.co.il/en/shown/?date={date_string}")
             self.zoomOut(50, 2)
-            try:
-                print("clicked 1a")
-                self.driver.execute_script("arguments[0].remove();", self.element("/html/body/div[5]/div[1]/div[2]/div[4]/div/div[1]"))
-                print("clicked 1b")
-            except:
-                print("clicked 2a")
-                self.driver.execute_script("arguments[0].remove();", self.element("/html/body/div[3]/div[1]/div[2]/div[4]/div/div[1]"))
-                print("clicked 2b")
+            self.driver.execute_script("arguments[0].remove();", self.element("/html/body/div[3]/div[1]/div[2]/div[4]/div/div[1]"))
             self.sleep(1)
 
-            for film_block in range(1, self.lenElements("/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div") + 1):
-                for film_card in range(1, self.lenElements(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div") + 1):
+            for film_block in range(1, self.lenElements("/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div") + 1):
+                for film_card in range(1, self.lenElements(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div") + 1):
                     self.release_year, self.runtime = None, None
 
-                    self.english_title = self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/h3/a").text.strip()
+                    self.english_title = self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/h3/a").text.strip()
                     if not re.search(r"[A-Za-z]", self.english_title):
-                        fallback_text = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
+                        fallback_text = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
                         if fallback_text:
                             words = fallback_text.split()
                             english_words = []
@@ -46,8 +39,8 @@ class TLVtheque(BaseCinema):
                                     break
                             self.english_title = " ".join(english_words) if english_words else None
 
-                    trying_year_1 = self.tryExceptNone(lambda: self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/p").get_attribute("textContent"))
-                    trying_year_2 = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
+                    trying_year_1 = self.tryExceptNone(lambda: self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/p").get_attribute("textContent"))
+                    trying_year_2 = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
                     for candidate in (trying_year_1, trying_year_2):
                         if candidate:
                             match = re.search(r"\b\d{4}\b", candidate)
@@ -55,8 +48,8 @@ class TLVtheque(BaseCinema):
                                 self.release_year = match.group(0)
                                 break
 
-                    trying_runtime_1 = self.tryExceptNone(lambda: self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/p").get_attribute("textContent"))
-                    trying_runtime_2 = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
+                    trying_runtime_1 = self.tryExceptNone(lambda: self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[1]/p").get_attribute("textContent"))
+                    trying_runtime_2 = self.tryExceptNone(lambda: " ".join(self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[2]/p").get_attribute("textContent").split()[:50]))
                     if trying_runtime_1 and "Length:" in trying_runtime_1:
                         match = re.search(r"Length:\s*(\d+)", trying_runtime_1)
                         if match:
@@ -67,9 +60,9 @@ class TLVtheque(BaseCinema):
                             self.runtime = match.group(1)
 
                     self.date_of_showing = datetime.strptime(date_string, "%Y-%m-%d").date().isoformat()
-                    self.english_href = self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[3]/div[1]/a").get_attribute("href")
+                    self.english_href = self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[3]/div[1]/a").get_attribute("href")
                     self.hebrew_href = self.english_href.replace("lang=en", "lang=he")
-                    self.showtime = self.element(f"/html/body/div[5]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[3]/div[1]/a/span").text.strip()
+                    self.showtime = self.element(f"/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[{film_block}]/div[{film_card}]/div[2]/div[3]/div[1]/a/span").text.strip()
                     self.screening_city = self.SCREENING_CITY
                     self.screening_type = "Regular"
 
