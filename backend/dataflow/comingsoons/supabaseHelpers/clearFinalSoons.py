e4f49c71b9fe3b8f43627b007b04fca9e0bcb656
JJ <jeremyjacob101@gmail.com>
2025-12-08 16:00:03 +0200

    Fixed table name mismatch

--- backend/dataflow/comingsoons/supabaseHelpers/clearFinalSoons.py
+++ backend/dataflow/comingsoons/supabaseHelpers/clearFinalSoons.py
@@ -9,4 +9,4 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 sb = create_client(url, key)
 
-sb.table("testingFinalSoons").delete().filter("id", "not.is", "null").execute()
+sb.table("testingFinalSoons").delete().filter("imdb_id", "not.is", "null").execute()
